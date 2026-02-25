34ce7912765e9920ff29c784dab31a9f3ef5c5dd
Akshaya S <717823p208@kce.ac.in>
2026-02-25 14:50:02 +0530

    initial commit - Weather ui project

---
+++ src/services/api.js
@@ -0,0 +1,84 @@
+import { useState } from "react";
+import "./App.css";
+import { WiThermometer, WiHumidity, WiBarometer, WiCloud } from "react-icons/wi";
+function App() {
+  const [city, setCity] = useState("");
+  const [weather, setWeather] = useState(null);
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState("");
+
+  const API_KEY = "a1302b74da784142c7f25a822b03ab70"; 
+  const getWeather = async () => {
+    if (!city) return;
+
+    setLoading(true);
+    setError("");
+    setWeather(null);
+
+    try {
+      const res = await fetch(
+        `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${API_KEY}&units=metric`
+      );
+
+      const data = await res.json();
+
+      if (!res.ok) {
+        throw new Error(data.message || "City not found");
+      }
+
+      setWeather(data);
+
+    } catch (err) {
+      setError(err.message);
+    }
+
+    setLoading(false);
+  };
+
+  return (
+    <div className="container">
+      <h1>Weather Dashboard</h1>
+
+      <div className="search-box">
+        <input
+          type="text"
+          placeholder="Enter city name"
+          value={city}
+          onChange={(e) => setCity(e.target.value)}
+        />
+        <button onClick={getWeather}>Get Weather</button>
+      </div>
+
+      {loading && <p className="loading">Loading...</p>}
+      {error && <p className="error">{error}</p>}
+
+      {weather && (
+  <div className="card">
+    <h2>{weather.name}, {weather.sys.country}</h2>
+
+    <p>
+      <WiThermometer size={24} /> 
+      <strong> Temperature:</strong> {weather.main.temp} °C
+    </p>
+
+    <p>
+      <WiHumidity size={24} /> 
+      <strong> Humidity:</strong> {weather.main.humidity}%
+    </p>
+
+    <p>
+      <WiBarometer size={24} /> 
+      <strong> Pressure:</strong> {weather.main.pressure} hPa
+    </p>
+
+    <p>
+      <WiCloud size={24} /> 
+      <strong> Condition:</strong> {weather.weather[0].description}
+    </p>
+  </div>
+)}
+    </div>
+  );
+}
+
+export default App;
